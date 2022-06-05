81313db96ce0f45f70bad9277b35ceb754deab10
Ralf Northman <ralf.northman@bredband.net>
2022-06-05 22:15:47 +0200

    Async attempt: but reads file before its written.

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,47 @@
+use anyhow::Result;
 use mimalloc::MiMalloc;
 use polars::prelude::*;
+use reqwest::header::ACCEPT;
+use reqwest::Client;
+use tokio::fs::File;
+use tokio::io::AsyncWriteExt;
 
 #[global_allocator]
 static GLOBAL: MiMalloc = MiMalloc;
 
-fn main() -> Result<()> {
-    let load = |s: &str| LazyCsvReader::new(s.into()).has_header(true).finish();
+async fn load(table: &str) -> Result<DataFrame> {
+    let client = Client::new();
+    let res = client
+        .get(format!("http://localhost:8070/api/tables{table}"))
+        .header(ACCEPT, "application/vnd.apache.arrow.file")
+        .send()
+        .await?
+        .bytes()
+        .await?;
 
-    let mut colors = load("csvs/colors.csv")?.collect()?;
+    let mut file = File::create("response").await?;
+    file.write_all(&res).await?;
+    let file = file.into_std().await;
+    let ipc = IpcReader::new(file).finish()?;
+    Ok(ipc)
+}
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    let mut colors = load("colors").await?;
     colors.try_apply("is_trans", |s| s.cast(&DataType::Categorical(None)))?;
-    let parts = load("csvs/parts.csv")?;
-    let inventory_parts = load("csvs/inventory_parts.csv")?;
-    let inventories = load("csvs/inventories.csv")?;
-    let sets = load("csvs/sets.csv")?;
-    let themes = load("csvs/themes.csv")?;
+    let parts = load("parts").await?;
+    let inventory_parts = load("inventory_parts").await?;
+    let inventories = load("inventories").await?;
+    let sets = load("sets").await?;
+    let themes = load("themes").await?;
 
     let some_glitters = colors
         .lazy()
         .filter(col("is_trans").eq(lit("t")))
         .sort_by_exprs(vec![col("name")], vec![false])
         .limit(5);
-    let rand_sets = sets.collect()?.sample_n(5, false, Some(42))?;
+    let rand_sets = sets.sample_n(5, false, Some(42))?;
 
     let all = collect_all([some_glitters])?;
     for df in all {
