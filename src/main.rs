a958fda749918c5c81d6d2ce1ed714c477994838
Ralf Northman <ralf.northman@bredband.net>
2022-06-06 17:31:22 +0200

    Same overflow-error when using lazy csv-reader from files on disc.

--- src/main.rs
+++ src/main.rs
@@ -1,42 +1,22 @@
 use anyhow::{Context, Result};
 use mimalloc::MiMalloc;
 use polars::prelude::*;
-use reqwest::blocking::Client;
-use reqwest::header::ACCEPT;
-use std::io::prelude::*;
-use std::io::Cursor;
 
 #[global_allocator]
 static GLOBAL: MiMalloc = MiMalloc;
 
-fn load(table: &str) -> Result<DataFrame> {
-    let client = Client::new();
-    let response = client
-        .get(format!("http://localhost:8070/api/tables/{table}"))
-        .header(ACCEPT, "application/parquet")
-        .send()
-        .with_context(|| format!("Failure sending http GET for '{table}'"))?
-        .bytes()
-        .with_context(|| format!("Failure taking bytes from response for '{table}'"))?;
-
-    let mut cursor = Cursor::new(Vec::new());
-    cursor
-        .write_all(&response)
-        .with_context(|| format!("Failure writing to cursor for '{table}'"))?;
-
-    let df = ParquetReader::new(cursor)
-        .finish()
-        .with_context(|| format!("Failure finishing parquet-read for '{table}'"))?;
+fn load(table: &str) -> Result<LazyFrame> {
+    let path = format!("csvs/{table}.csv");
+    let df = LazyCsvReader::new(path).has_header(true).finish()?;
     Ok(df)
 }
 
 fn main() -> Result<()> {
-    let mut colors = load("colors")?;
+    let colors = load("colors")?;
+    let mut colors = colors.collect()?;
     colors
         .try_apply("is_trans", |s| s.cast(&DataType::Categorical(None)))
-        .context(format!(
-            "Failure casting colors.is_trans to categorical type."
-        ))?;
+        .context("Failure casting colors.is_trans to categorical type.".to_string())?;
     let parts = load("parts")?;
     let inventory_parts = load("inventory_parts")?;
     let inventories = load("inventories")?;
@@ -48,23 +28,18 @@ fn main() -> Result<()> {
         .filter(col("is_trans").eq(lit("t")))
         .sort_by_exprs(vec![col("name")], vec![false])
         .limit(5);
-    let rand_sets = sets.sample_n(5, false, Some(42))?;
+    let rand_sets = sets.clone().collect()?.sample_n(5, false, Some(42))?;
 
     let joined = sets
-        .lazy()
         .rename(&["name"], &["set_name"])
         .inner_join(
-            inventories.lazy().rename(&["id"], &["inventory_id"]),
+            inventories.rename(&["id"], &["inventory_id"]),
             col("set_num"),
             col("set_num"),
         )
+        .inner_join(inventory_parts, col("inventory_id"), col("inventory_id"))
         .inner_join(
-            inventory_parts.lazy(),
-            col("inventory_id"),
-            col("inventory_id"),
-        )
-        .inner_join(
-            parts.lazy().rename(&["name"], &["part_name"]),
+            parts.rename(&["name"], &["part_name"]),
             col("part_num"),
             col("part_num"),
         )
