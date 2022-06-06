93bac843d59ab44892ced5a1ffd92870955050a1
Ralf Northman <ralf.northman@bredband.net>
2022-06-06 17:01:45 +0200

    Selects. Works with downsampled 'sets' but overflow-errors with full 'sets'.

--- src/main.rs
+++ src/main.rs
@@ -24,31 +24,72 @@ fn load(table: &str) -> Result<DataFrame> {
         .write_all(&response)
         .with_context(|| format!("Failure writing to cursor for '{table}'"))?;
 
-    let ipc = ParquetReader::new(cursor)
+    let df = ParquetReader::new(cursor)
         .finish()
         .with_context(|| format!("Failure finishing parquet-read for '{table}'"))?;
-    Ok(ipc)
+    Ok(df)
 }
 
 fn main() -> Result<()> {
     let mut colors = load("colors")?;
-    colors.try_apply("is_trans", |s| s.cast(&DataType::Categorical(None)))?;
+    colors
+        .try_apply("is_trans", |s| s.cast(&DataType::Categorical(None)))
+        .context(format!(
+            "Failure casting colors.is_trans to categorical type."
+        ))?;
     let parts = load("parts")?;
     let inventory_parts = load("inventory_parts")?;
     let inventories = load("inventories")?;
     let sets = load("sets")?;
 
     let some_glitters = colors
+        .clone()
         .lazy()
         .filter(col("is_trans").eq(lit("t")))
         .sort_by_exprs(vec![col("name")], vec![false])
         .limit(5);
     let rand_sets = sets.sample_n(5, false, Some(42))?;
 
-    let all = collect_all([some_glitters])?;
-    for df in all {
+    let joined = sets
+        .lazy()
+        .rename(&["name"], &["set_name"])
+        .inner_join(
+            inventories.lazy().rename(&["id"], &["inventory_id"]),
+            col("set_num"),
+            col("set_num"),
+        )
+        .inner_join(
+            inventory_parts.lazy(),
+            col("inventory_id"),
+            col("inventory_id"),
+        )
+        .inner_join(
+            parts.lazy().rename(&["name"], &["part_name"]),
+            col("part_num"),
+            col("part_num"),
+        )
+        .inner_join(
+            colors
+                .lazy()
+                .rename(&["id", "name"], &["color_id", "color_name"]),
+            col("color_id"),
+            col("color_id"),
+        )
+        .select(&[
+            col("set_num"),
+            col("set_name"),
+            col("year"),
+            col("part_num"),
+            col("part_name"),
+            col("color_name"),
+            col("quantity"),
+        ])
+        .limit(5);
+
+    let mut collected = collect_all([some_glitters, joined])?;
+    collected.push(rand_sets);
+    for df in collected {
         println!("{}", df);
     }
-    println!("{}", rand_sets);
     Ok(())
 }
