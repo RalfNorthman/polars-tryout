7d99fbdd889f9656470810346e3960c9c92d0ce2
Ralf Northman <ralf.northman@bredband.net>
2022-06-18 14:18:47 +0200

    Minimal example + fixed in polars master.

--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,22 @@
-use anyhow::Result;
 use mimalloc::MiMalloc;
-use modulo::Mod;
 use polars::prelude::*;
 
 #[global_allocator]
 static GLOBAL: MiMalloc = MiMalloc;
 
 fn main() -> Result<()> {
-    let a: Vec<i8> = ((-127i8)..=127).filter(|n| n.modulo(7) == 0).collect();
-    let b: Vec<i8> = ((-127i8)..=127).filter(|n| n.modulo(3) == 0).collect();
-
     let df1 = df![
-        "a" => a,
+        "a" => [-6_i8, -3, 0],
     ]?
     .lazy();
 
     let df2 = df![
-        "b" => b,
+        "b1" => [-6_i8, -4, -2, 0],
+        "b2" => [-6_i8, -4, -2, 0],
     ]?
     .lazy();
 
-    let df = df1.inner_join(df2, col("a"), col("b")).collect()?;
-
-    dbg!((-119i8).modulo(7) == 0);
-    dbg!((-119i8).modulo(3) == 0);
-
-    dbg!(-7i8.modulo(7) == 0);
-    dbg!(-3i8.modulo(3) == 0);
+    let df = df1.inner_join(df2, col("a"), col("b1")).collect()?;
 
     println!("{df}");
 
